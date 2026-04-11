807f1941c9d952259e1cdb35ecd973d02dae4fd1
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2026-04-11 22:13:05 +0530

    working on the Variables-Conditions-Loops practice questions

--- Variables-Conditions-Loops/script.js
+++ Variables-Conditions-Loops/script.js
@@ -60,3 +60,81 @@ i--;
 
 
 //3.	Print even numbers 1–100
+//4.	Print odd numbers
+let odd=[]
+let even=[]
+
+/*console.log(odd,even)
+
+
+for(let i=1; i<=100; i++){
+if(i%2===0){
+even.push(i);
+} else {
+odd.push(i);
+}
+}
+console.log(odd,even)
+*/
+
+
+/*
+let i=1;
+
+while(i<=100){
+if(i%2===0){
+even.push(i)
+}else{
+odd.push(i);
+}
+i++
+}
+console.log(odd,even)
+*/
+
+
+
+
+
+
+//5.	Sum of 1–100
+
+let sum=0
+
+/*
+for(let i=1; i<=100; i++){
+sum+=i
+}
+console.log(sum)
+*/
+
+
+/*
+let i=1
+while(i<=100){
+sum+=i;
+i++;
+}
+console.log(sum)
+*/
+
+
+
+6.	Factorial of a number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
