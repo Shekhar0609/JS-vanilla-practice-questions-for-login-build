eae50d912d02490b82e7be3c76d1a697995f4220
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2026-04-11 20:10:23 +0530

    working on the Variables-Conditions-Loops practice questions

---
+++ Variables-Conditions-Loops/script.js
@@ -0,0 +1,62 @@
+//1.	Print 1–100
+
+/*
+for(let i=1; i<=100; i++){
+console.log(i);
+}
+*/
+
+
+/*
+let i=1
+
+while(i<=100){
+console.log(i);
+i++;
+}
+*/
+
+
+/*
+let i=1;
+do{
+console.log(i)
+i++
+} while(i<=100)
+*/
+
+
+
+
+
+//2.	Print 100–1
+
+/*
+for(let i=100; i>=1; i--){
+console.log(i);
+}
+*/
+
+
+/*
+let i=100
+while(i>=1){
+console.log(i);
+i--
+}
+*/
+
+
+/*
+let i=100
+do{
+console.log(i);
+i--;
+} while(i>=1)
+*/
+
+
+
+
+
+//3.	Print even numbers 1–100
