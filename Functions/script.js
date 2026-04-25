ae4fddcf9201fb1ff196519d2f791b9bd753248d
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2026-04-26 02:27:19 +0530

    working on the Variables-Conditions-Loops practice questions

--- Functions/script.js
+++ Functions/script.js
@@ -1,1 +1,19 @@
 // 21.	Add two numbers
+
+/*
+let addTwoNumber = (a, b) => {
+return a + b;
+}
+
+console.log(addTwoNumber(2,6))
+*/
+
+// 22.	Subtract two numbers
+
+/*
+let subtractTwoNumber = (a, b) => {
+return a - b;
+}
+
+console.log(subtractTwoNumber(2,6))
+*/
